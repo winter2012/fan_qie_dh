b2c415fe2f9ae262b87f798eb44d01ea49da930e
winter2012 <winter2012102@gmail.com>
2019-07-16 19:30:48 +0800

    first commit

---
+++ Public/Common/config.db.php
@@ -0,0 +1,13 @@
+<?php
+ return array (
+  'DB_HOST' => 'localhost',
+  'DB_PORT' => '3306',
+  'DB_USER' => 'fanqiedh_com',
+  'DB_NAME' => 'fanqiedh_com',
+  'DB_PWD' => 'aFmDMcjB85k2dMr2',
+  'DB_PREFIX' => 'cc_',
+  'ROOT_FILE' => '/',
+  'DEFAULT_THEME' => 'Black',
+  'URL_MODEL' => '0',
+)
+?>
